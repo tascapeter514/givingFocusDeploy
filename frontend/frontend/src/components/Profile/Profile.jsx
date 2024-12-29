48555a39df65df9f7b7f16311f77e38b06562b1d
petertasca <tascapeter@gmail.com>
2024-12-29 11:22:56 -0500

    mary louise pre flex box

--- frontend/frontend/src/components/Profile/Profile.jsx
+++ frontend/frontend/src/components/Profile/Profile.jsx
@@ -4,15 +4,19 @@ import './Profile.css'
 export default function Profile() {
     return(
         <section className='maryLouise-container'>
-                <div className="mary-louise-image-wrapper">
-                    <img src={require("./assets/mary-louise-photo.jpg")} alt="mary-louise-photo" />
-                    <div className="maryLouise-card-background"></div>
-                </div>
-                <article class="maryLouise-text-wrapper">
+            <article className="mary-louise-header">
                 <hr />
                 <h2>Meet Mary Louise</h2>
-                <h5>Mary Louise Fazzano is the President of Giving Focus LLC® 
+                <h5>Mary Louise Fazzano is the President of Giving Focus LLC®
                 a career consulting and advisory firm serving individuals at the start of their career, in transition, or seeking career advancement.</h5>
+                    <div className="mary-louise-image-wrapper">
+                        <img src={require("./assets/mary-louise-photo.jpg")} alt="mary-louise-photo" />
+                        <div className="maryLouise-card-background"></div>
+                    </div>
+            </article>
+                <article class="maryLouise-text-wrapper">
+                
+                
                 <p>
                 She is known as an expert listener with clear, practical suggestions. More than 400 satisfied clients have recommended Giving Focus to colleagues and friends globally.
                 Mary Louise creates targeted and customized resumes and biographies for clients pursuing unique professional opportunities. 
